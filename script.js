1de67e0b34d4a78707dcd6f55439fa7a07a68cff
Poovarasanelango6 <poovarasane161@gmail.com>
2026-08-27 23:31:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Variable
+let studentName = "Poovarasan";
+let age = 29;
+let mark = 90;
+console.log(studentName, age, mark);
+
+//name using prompt()
+let PersonName = prompt("Enter your name:");
+alert("Your name is " + PersonName);
+
+//age using prompt()
+let PersonAge = prompt("Enter your age:");
+console.log("Your age is " + PersonAge);
+
+//confirm()
+let result = confirm("Do you want to continue?");
+console.log(result); 
+
+//WriteIn
+let Displayname = prompt("Enter your name:");
+document.writeln("Hello, " + Displayname);
